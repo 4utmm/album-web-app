b0c1584dcf121a0f05a43918e89934884624d51f
4utumm <autumntovey@gmail.com>
2025-05-07 13:41:18 -0300

    Added default order game class

---
+++ album-app/func/game/NormalOrderGame.cs
@@ -0,0 +1,54 @@
+class NormalOrderGame : IOrderGame {
+
+    public NormalOrderGame() { //NOT READY
+
+    }
+
+    public Boolean isOrderCorrect(){ //NOT READY
+        return true;
+    }
+
+    public int[] incorrectTrackIndex(){ //NOT READY
+        return null;
+    }
+
+    public int getTrackCount(){ //NOT READY
+        return 0;
+    }
+
+    public string[] getTrackList(){ //NOT READY
+        return null;
+    }
+
+    public string[] getShuffledTrackList(){ //NOT READY
+        return null;
+    }
+
+    public string getAlbumArt(){  //NOT READY
+        return null;
+    }
+
+    public string getAlbumName(){ //NOT READY
+        return null;
+    }
+
+    public string[] getAlbumArtists(){ //NOT READY
+        return null;
+    }
+
+    public string getAlbumReleaseDate(){ //NOT READY
+        return null;
+    }
+
+    public void placeTrack(){ //NOT READY
+
+    }
+
+    public void removeTrack(){ //NOT READY
+
+    }
+
+    public void solve(){ //NOT READY
+        
+    }
+}
